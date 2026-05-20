8d485dbc0354bf725d1cc8a8fb66a8a50788d799
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-05-20 14:09:35 -0300

    update: Envio

--- src/main/java/com/elmensajero/demo/domain/entity/Envio.java
+++ src/main/java/com/elmensajero/demo/domain/entity/Envio.java
@@ -1,14 +1,23 @@
 package com.elmensajero.demo.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import com.elmensajero.demo.domain.enums.EstadoEnvio;
+import com.elmensajero.demo.domain.enums.ZonaCaba;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "envios")
