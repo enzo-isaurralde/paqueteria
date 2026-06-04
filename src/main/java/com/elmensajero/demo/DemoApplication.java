dff4a3d8bd09b82779c8e979d6bd8b291468e2f7
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-06-04 09:59:25 -0300

    feat: configuración principal y página de inicio

--- src/main/java/com/elmensajero/demo/DemoApplication.java
+++ src/main/java/com/elmensajero/demo/DemoApplication.java
@@ -2,12 +2,13 @@ package com.elmensajero.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.elmensajero.demo.domain.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
